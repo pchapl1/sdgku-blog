3b11abe8851dd0e0746ce82b6d64d47c19af2416
Phil Chaplin <pchapl1@gmail.com>
2022-05-02 19:41:43 -0600

    changed created on field to datetime

--- posts/models.py
+++ posts/models.py
@@ -7,7 +7,7 @@ class Post(models.Model):
     title = models.CharField(max_length=45)
     body = models.TextField(max_length=1000)
     author = models.ForeignKey('auth.User', on_delete=models.CASCADE)
-    created_on = models.DateField(auto_now_add=True)
+    created_on = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.title
